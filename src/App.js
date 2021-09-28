4c2f07e74565218c3097a0e4d436c1c0c95dfe8b
jina <god48cja@naver.com>
2021-09-28 20:19:43 +0900

    React First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  //props를 쓰기위한 코드
+  //어떤 Component가 실행될 때 render함수보다 먼저 실행이 되면서 그 Component를 초기화 시켜주고싶은 코드는 constructor 를 만들고 안에다 코드를 작성한다.
+  constructor(props){
+    super(props);
+    this.state = {
+      //state값을 초기화한다. subject의 값을 state화 시킬거임, subject의 프로퍼티(props)값으로 
+      subject : {title : 'WEB', sub: 'World wide web!'}
+    }
+  }
+  render(){
+    return(
+      <div className="APP">
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc=" HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
